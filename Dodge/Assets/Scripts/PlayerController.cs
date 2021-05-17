94abe9f42f2741da23700628168fa3f4afe35d67
연제무 <brian2189@hanmail.net>
2021-05-17 11:51:47 +0900

    닷지 플레이어 1.0
    닷지 플레읻어 1.0

---
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerrRigidbody;
+    public float speed = 80f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerrRigidbody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerrRigidbody.velocity = newVelocity;
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+
+        GameManager gameManager = FindObjectOfType<GameManager>();
+
+        gameManager.Endgame();
+    }
+}
